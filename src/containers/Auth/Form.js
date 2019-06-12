2b45619b7b043f420c94f3f64ba0f56c1e2dd91c
hayat <hayattalaat50@gmail.com>
2019-06-12 15:53:00 +0200

    regular stylesheet

--- src/containers/Auth/Form.js
+++ src/containers/Auth/Form.js
@@ -1,24 +1,21 @@
 import React, {Component} from 'react';
 import Input from '../../components/UI/Input/Input';
-import classes from './Form.css'
+import './Form.css';
 
 class Form extends Component {
     render() {
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="text" name="email" placeholder="Your Mail" />
+                <Input className="Input" inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input className="Input" inputtype="input" type="email" name="email" placeholder="Your Mail" />
                 <button>ORDER</button>
             </form>
         );
 
         return(
-            <div className={classes.FormData}>
+            <div className="FormData">
                 <h3>Login</h3>
                 {form}
-                <div className={classes.Test}>test</div>
             </div>
         );
     }
